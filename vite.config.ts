f484ae738020df4f9385c3efa4129251c7216357
Abreu-Renan <renan6basket@hotmail.com>
2026-08-21 14:59:12 -0300

    atl

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 
-const isGitHubPages = process.env.GITHUB_PAGES === "true";
+const isGitHubPages = process.env["GITHUB_PAGES"] === "true";
 
 export default defineConfig({
   base: isGitHubPages ? "./" : "/",
